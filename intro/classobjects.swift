8365de7a55a3d00193ccc05df9ee6578726541af
Kedar <kedardeore@gmail.com>
2016-01-13 00:16:00 +0530

    Classes and objects

---
+++ intro/classobjects.swift
@@ -0,0 +1,50 @@
+class Shape {
+    var sides: Int = 0
+    var name: String
+
+    func describe() -> String {
+        return "A \(name) with \(sides) sides"
+    }
+
+    init(_ name: String) {
+        self.name = name
+    }
+
+    deinit {
+        print("Cleaning up \(name)…")
+    }
+}
+
+var shape = Shape("foo")
+print(shape.sides)
+print(shape.describe())
+
+
+class Square: Shape {
+    var sideLength: Double
+    // base class property getter / setter can be overridden
+    override var sides: Int {
+        get { return 4 }
+        // must have both getter & setter
+        set {
+            // `newValue` holds value that was set
+        }
+    }
+
+    init(_ name: String, _ sideLength: Double) {
+        // properties new in subclass must be defined before .init call
+        self.sideLength = sideLength
+        super.init(name)
+        // properties defined in base may be modified only after super.init
+        self.sides = 4  // doesn't matter what is set here, see get / set
+    }
+
+    // function signature must match base class
+    override func describe() -> String {
+        return "A \(name) with \(sides) sides of length \(sideLength) units"
+    }
+}
+
+
+var box = Square("square", 4.0)
+print(box.describe())
