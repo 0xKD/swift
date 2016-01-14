9450d13ec52838967322922dd68faa10393bbe39
Kedar <kedardeore@gmail.com>
2016-01-15 00:19:14 +0530

    willSet, didSet, alternate parameter name

--- intro/classobjects.swift
+++ intro/classobjects.swift
@@ -21,7 +21,17 @@ print(shape.describe())
 
 
 class Square: Shape {
-    var sideLength: Double
+    var sideLength: Double {
+        // property observer (to monitor changes)
+         willSet {
+            print("Run before setting sideLength to \(newValue)")
+        }
+        // observers do not go well with getter / setter (try adding getter / setter to this property)
+        didSet {
+            print("Run after settings sideLength to \(sideLength) (previously \(oldValue))")
+        }
+    }
+
     // base class property getter / setter can be overridden
     override var sides: Int {
         get { return 4 }
@@ -36,15 +46,26 @@ class Square: Shape {
         self.sideLength = sideLength
         super.init(name)
         // properties defined in base may be modified only after super.init
-        self.sides = 4  // doesn't matter what is set here, see get / set
+        self.sides = 9  // doesn't matter what is set here, see get / set
     }
 
     // function signature must match base class
     override func describe() -> String {
         return "A \(name) with \(sides) sides of length \(sideLength) units"
     }
+
+    // second name for method parameter
+    // `extraText` used when calling, `text` used inside the method
+    func describe(extraText text:String?) -> String {
+        if let extras = text {
+            return "\(extras)"
+        } else {
+            return "A \(name) with \(sides) sides of length \(sideLength) units"
+        }
+    }
 }
 
 
 var box = Square("square", 4.0)
-print(box.describe())
+print(box.describe(extraText: "Hello sir!"))
+box.sideLength = 5.0
