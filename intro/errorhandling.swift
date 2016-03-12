4d34818ea4868df758363ffe36e23c1aa07d8d26
Kedar <kedardeore@gmail.com>
2016-03-12 07:49:37 +0530

    Error handling

---
+++ intro/errorhandling.swift
@@ -0,0 +1,31 @@
+// ErrorType is built-in protocol that represents errors
+enum MyBandError: ErrorType {
+    case NoMic
+    case TooLoud
+    case MissingSlim
+}
+
+func playSong(songName: String) throws -> String {
+    if songName.hasSuffix("mic") {
+        throw MyBandError.NoMic
+    } else if songName.hasSuffix("up") {
+        throw MyBandError.TooLoud
+    }
+    return "∞ \(songName) ∞"
+}
+
+do {
+    let song = try playSong("turn it up")  // put something else here
+    print(song)
+} catch MyBandError.NoMic {  // catch specific error
+    print("Got no mic!")
+} catch let myberr as MyBandError {  // catch errors of a specific type
+    print("Get it together band! (\(myberr))")
+} catch {  // any other error
+    print("Something went wrong! (\(error))")
+}
+
+// convert result to optional. Any error is discarded and nil returned
+if let s = try? playSong("hello") {
+    print("Now Playing: \(s)")
+}
